47f6736f43c9dcece789ecbedbc7848e9b46289e
akucherevskiy <a.kucherevskiy@gmail.com>
2016-01-27 14:52:03 +0200

    init + +fosuser+apidoc+fosrest

---
+++ src/AppBundle/Form/RegistrationType.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anton
+ * Date: 27.01.16
+ * Time: 14:44
+ */
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name');
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+}
